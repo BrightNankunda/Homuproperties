68ddd72e1ca88b7c8bdb964c04fac2a38c09d183
BrightNankunda <67958069+BrightNankunda@users.noreply.github.com>
2021-05-05 21:46:10 +0300

    Form Requests

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -94,27 +94,6 @@ class ImageController extends Controller
     }
     public function upload(ImageRequest $request)
     {
-        // $this->validate($request,[
-        //     'name' => 'required|string',
-        //     'type' => 'required|string',
-        //     'category' => 'required|string',
-        //     'location' => 'required|string',
-        //     'address' => 'required|string',
-        //     'street' => 'required|string',
-        //     'description' => 'required|string',
-        //     'rooms' => 'required|string',
-        //     'floor' => 'required|string',
-        //     'rent' => 'required|string',
-        //     //'amount' => 'reqiured|string',
-        //     'contact' => 'required|string',
-        //     'front' => 'required|image|mimes:jpg,jpeg',
-        //     'image' => 'required|image|mimes:jpg,jpeg',
-        //     'photo' => 'required|image|mimes:jpg,jpeg',
-        //     'imageone' => 'required|image|mimes:jpg,jpeg',
-        //     'imagetwo' => 'required|image|mimes:jpg,jpeg',
-        // ]);
-
-    
         // if($request->hasFile('image')){
         //     $name = time().".".$request->file('image')->getClientOriginalName();
         //     $request->file('image')->move(public_path('images'), $name);
